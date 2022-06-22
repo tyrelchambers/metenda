e2f03d8f991f73fb555ab7fff9e2e715afc78146
Tyrel Chambers <tychambers3@gmail.com>
2022-06-21 20:52:17 -0400

    implement logout

--- app/layout/DashHeader.tsx
+++ app/layout/DashHeader.tsx
@@ -1,12 +1,15 @@
+import { Form, Link } from "@remix-run/react";
+
 import DashNav from "./DashNav";
-import { Link } from "@remix-run/react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import React from "react";
+import { faArrowRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 
 const DashHeader = () => {
   return (
-    <header className="sticky top-4 h-full w-full max-w-[300px] rounded-3xl bg-gray-800 p-4 shadow-lg">
-      <h2 className="text-xl tracking-wider text-gray-100">MetaClimb</h2>
+    <header className="sticky top-4 flex h-full w-full max-w-[300px] flex-col justify-between rounded-3xl bg-gray-800 p-4 shadow-lg">
       <nav className="flex flex-col">
+        <h2 className="text-xl tracking-wider text-gray-100">MetaClimb</h2>
         <Link
           to="/task/new"
           className="mt-6 mb-2 w-full rounded-lg bg-indigo-500 p-2 text-center text-white transition-all hover:bg-indigo-600"
@@ -22,6 +25,18 @@ const DashHeader = () => {
         <hr className="mb-6 border-gray-600" />
         <DashNav />
       </nav>
+      <Form action="/logout" method="post">
+        <button
+          type="submit"
+          className="flex items-center gap-6  text-gray-400 transition-all hover:text-white"
+        >
+          <FontAwesomeIcon
+            icon={faArrowRightFromBracket}
+            style={{ width: "17px" }}
+          />{" "}
+          Sign out
+        </button>
+      </Form>
     </header>
   );
 };
--- app/session.server.ts
+++ app/session.server.ts
@@ -1,8 +1,8 @@
 import { createCookieSessionStorage, redirect } from "@remix-run/node";
-import invariant from "tiny-invariant";
 
 import type { User } from "~/models/user.server";
 import { getUserById } from "~/models/user.server";
+import invariant from "tiny-invariant";
 
 invariant(process.env.SESSION_SECRET, "SESSION_SECRET must be set");
 
@@ -90,6 +90,8 @@ export async function createUserSession({
 
 export async function logout(request: Request) {
   const session = await getSession(request);
+  console.log(session);
+
   return redirect("/", {
     headers: {
       "Set-Cookie": await sessionStorage.destroySession(session),
