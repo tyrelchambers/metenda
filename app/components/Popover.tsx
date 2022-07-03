72fdfd929cbe79ed42cde757ce4511badade49fc
Tyrel Chambers <tychambers3@gmail.com>
2022-07-03 14:36:16 -0400

    .

---
+++ app/components/Popover.tsx
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+import { Popover as MantinePopover } from "@mantine/core";
+
+interface Props {
+  triggerLabel: string | React.ReactNode;
+  children: React.ReactNode;
+  placement?: "start" | "center" | "end";
+}
+
+const Popover = ({ children, triggerLabel, placement = "start" }: Props) => {
+  const [opened, setOpened] = useState(false);
+
+  return (
+    <MantinePopover
+      opened={opened}
+      onClose={() => setOpened(false)}
+      target={
+        <button
+          onClick={() => setOpened((o) => !o)}
+          className=" text-sm text-gray-600"
+          type="button"
+        >
+          {triggerLabel}
+        </button>
+      }
+      width={300}
+      position="bottom"
+      placement={placement}
+      withArrow
+      shadow="lg"
+      zIndex={10}
+    >
+      {children}
+    </MantinePopover>
+  );
+};
+
+export default Popover;
